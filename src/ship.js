1c989b0c015829e34bdfd4bbfa30ca5b41bd4351
ranforrestren <ranforrestren@gmail.com>
2023-07-14 01:49:28 -0400

    shipFactory passes intial set of tests

--- src/ship.js
+++ src/ship.js
@@ -1,9 +1,21 @@
 
 const shipFactory = (length) => {
-    const addHit = function() {};
+    if (typeof length != "number") {
+        throw new Error("Number expected as parameter!");
+    }
+    let hits = 0;
+    let sunkStatus = false;
+
+    const addHit = function() {
+        hits += 1;
+    };
     // adds a hit 
-    const getHits = function() {};
-    const isSunk = function() {};
+    const getHits = function() {
+        return hits;
+    };
+    const isSunk = function() {
+        return hits === length;
+    };
     // just reports whether ship is destroyed
     return { addHit, getHits, isSunk }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -10,17 +10,13 @@ test('fails to construct ship (no argument)', () => {
 
 test('expect getHit() to return 0 on new ship', () => {
     const ship = shipFactory(3);
-    expect( () => {
-        ship.getHits();
-    }).toBe(0);
+    expect( ship.getHits() ).toBe(0);
 })
 
 test('expect addHit() to add a hit on a ship', () => {
     const ship = shipFactory(3);
     ship.addHit();
-    expect( () => {
-        ship.getHits();
-    }).toBe(1);
+    expect( ship.getHits() ).toBe(1);
 })
 
 test('expect isSunk() to be true on 3 hits / 3 length', () => {
@@ -28,10 +24,14 @@ test('expect isSunk() to be true on 3 hits / 3 length', () => {
     ship.addHit();
     ship.addHit();
     ship.addHit();
-    expect( () => {
-        ship.getHits();
-    }).toBe(3);
-    expect( () => {
-        ship.isSunk();
-    }).toBe(true);
+    expect( ship.getHits() ).toBe(3);
+    expect( ship.isSunk() ).toBe(true);
+})
+
+test('expect isSunk() to be false on 2 hits / 3 length', () => {
+    const ship = shipFactory(3);
+    ship.addHit();
+    ship.addHit();
+    expect( ship.getHits() ).toBe(2);
+    expect( ship.isSunk() ).toBe(false);
 })
